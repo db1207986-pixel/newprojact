9823af5ab5cbe3ecd40d1f463b7cc253462e76e9
db1207986-pixel <db1207986@gmail.com>
2026-06-08 16:08:20 +0700

    Add files via upload

---
+++ User.js
@@ -0,0 +1,19 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+  number: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  balance: {
+    type: Number,
+    default: 0
+  }
+}, { timestamps: true });
+
+module.exports = mongoose.model('User', userSchema);
